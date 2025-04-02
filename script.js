432a09427c630b1884772ea274bc908f5ffd7c19
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-04-02 15:11:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url){
+	return new Promise((resolve,reject)=>{
+		const img =new Image();
+		img.src=url;
+		img.onload=()=>resolve(img);
+		img.onerror=()=>reject(`failed to load image : ${url}`);
+		
+	})
+}
+
+async function downloadImages(){
+	let loading = document.createElement("span");
+	   
+	output.innerHTML="";
+	 loading.style.display = "block";
+	loading.innerText="loading...."
+	output.appendChild(loading)
+	try{
+		const imageElements =await Promise.all(images.map(image=>downloadImage(image.url)))
+		imageElements.forEach(img=>output.appendChild(img));
+		
+	}
+	catch(error){
+		let errordiv = document.createElement("span");
+		errordiv.innerText=error.message;
+		output.appendChild(errordiv)
+	} finally{
+		loading.style.display="none"
+	}
+}
+
+btn.addEventListener("click",downloadImages)
+
+
+btn.addEventListener("click",()=>{
+	// alert("hello");
+})
